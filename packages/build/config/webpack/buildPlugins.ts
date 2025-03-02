3c839dbd1f00deb118b4ca2f232fad83565f0881
Ilia <hikikomori7@gmail.com>
2025-03-02 09:53:42 +0100

    feat: DotenvWebpack plugin settings were added to buildPlugins

--- packages/build/config/webpack/buildPlugins.ts
+++ packages/build/config/webpack/buildPlugins.ts
@@ -1,29 +1,47 @@
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import DotenvWebpack from 'dotenv-webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
 import webpack from 'webpack';
 
 import { SharedPathPlugin } from './plugins/sharedPathPlugin';
 import { BuildOptions } from './types/config';
+
 
 export function buildPlugins({
     paths,
     isDev,
     project,
     apiUrl,
+    rootPath,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
     const plugins = [
+        // HTML document generation
         new HtmlWebpackPlugin({ template: paths.html }),
+        // CSS files generation
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
+        // Progress bar in the terminal
         new webpack.ProgressPlugin(),
+        // Environment variables
+        new DotenvWebpack({
+            path: path.join(rootPath, '.env.local'),
+            safe: path.join(rootPath, 'env.example'),
+            systemvars: true,
+            allowEmptyValues: true,
+            silent: true,
+            defaults: true,
+        }),
+        // Global variables
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
             __PROJECT__: JSON.stringify(project),
             __API_URL__: JSON.stringify(apiUrl),
         }),
+        // Plugin to correctly resolve absolute paths in the project
         SharedPathPlugin,
     ];
 
