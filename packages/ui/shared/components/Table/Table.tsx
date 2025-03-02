921846761b97890023a94e38f3349032d85fd563
Ilia <hikikomori7@gmail.com>
2025-03-02 11:43:24 +0100

    feature: Table component was added to ui library

---
+++ packages/ui/shared/components/Table/Table.tsx
@@ -0,0 +1,38 @@
+import {
+    Data,
+    TableNode
+} from '@table-library/react-table-library';
+import { CompactTable } from '@table-library/react-table-library/compact';
+import { Column } from '@table-library/react-table-library/types/compact';
+import {
+    memo 
+} from 'react';
+
+export interface IProps<TData extends TableNode> {
+    /**
+     * Data to be displayed in the table.
+     */
+    data: Data<TData>;
+    /**
+     * Columns to be displayed in the table.
+     */
+    columns: Column<TData>[];
+}
+
+/**
+ * Table component with generic data type.
+ * @param props - Props for the Table component.
+ * @returns Table component.
+ */
+export const Table = memo(<TData extends TableNode>(props: IProps<TData>) => {
+    const {
+        data, columns, 
+    } = props;
+
+    return (
+        <CompactTable
+            data={data}
+            columns={columns}
+        />
+    );
+});
